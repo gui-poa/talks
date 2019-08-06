6ca03fb419ec62ba30be275bcc63923106a6a599
Guilherme <guilhermezz@gmail.com>
2019-08-06 18:32:09 -0300

    handler connection

--- azion2019/netinfo.js
+++ azion2019/netinfo.js
@@ -1,1 +1,27 @@
-console.log("teste")
+function netinfo()
+{
+	var elNetinfo = document.getElementById("netinfo");
+
+	var init = function() {
+
+		writesNetInfo();
+
+		navigator.connection.addEventListener('change', writesNetInfo);
+	}
+
+	function writesNetInfo()
+	{
+		var connection = navigator.connection;
+
+		var text="";
+
+		text = "Effective Type: "+connection.effectiveType+"<br />";
+		text += "Download: "+connection.downlink+"Mbps <br />";
+		text += "RTT: "+connection.rtt+"ms <br />";
+		text += "Type: "+connection.type;
+
+		elNetinfo.innerHTML = text;
+	}
+
+	init();
+}
